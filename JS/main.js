c8ce22b478d51f81e3ca2aeb05252251ba72734d
monchi316 <monchi316@gmail.com>
2020-06-01 00:20:53 +0900

    課題1-2完了

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,13 @@
 'use strict'
 
-課題1-2準備
+let answer = 0;
+
+while (answer < 100) {
+  var result = prompt('日本の首都は？');
+  if (result === "東京" || result === "とうきょう" || result === "トウキョウ" || result === "ﾄｳｷｮｳ" || result === "tokyo" ) {
+    alert('正解！');
+  } else {
+    alert('不正解！');
+  }
+  answer++;
+}
