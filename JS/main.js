f8a59f617c94a985210dc7b7b06568a928e95fb4
monchi316 <monchi316@gmail.com>
2020-06-10 00:13:36 +0900

    修正しました

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
 'use strict'
 
-let answer = 0;
-
-while (answer < 100) {
-  var result = prompt('日本の首都は？');
-  if (result === "東京" || result === "とうきょう" || result === "トウキョウ" || result === "ﾄｳｷｮｳ" || result === "tokyo" ) {
-    alert('正解！');
-  } else {
-    alert('不正解！');
+{
+  const syuto = ['tokyo', '東京', 'トウキョウ']
+  while (true) {
+    if (syuto.indexOf(prompt('日本の首都は？')) !== -1) {
+      alert('正解です！');
+    } else {
+      alert('不正解です！');
+    }
   }
-  answer++;
+
 }
